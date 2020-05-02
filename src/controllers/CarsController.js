b490da571f191c039311be40029d2040d1e0a9b7
ederpbj <ederpbj@gmail.com>
2020-05-02 20:48:14 -0300

    Desvendando o Redux #6: Desenvolvendo o Back-End

--- src/controllers/CarsController.js
+++ src/controllers/CarsController.js
@@ -1,10 +1,27 @@
 const Car = require('../models/Car')
 class CarsController {
+    constructor(dao){
+        this.dao = dao
+
+        this.index = this.index.bind(this)
+        this.create = this.create.bind(this)
+    }
+    
+    // index = async (req, res) => {
     async index(req, res){
+        console.log(this.dao)
+
         const cars = await Car.find()
         return res.json(cars)
-        // return res.json({message: 'Cars...'})
+        // return res.json({message: 'Cars...'}) //Teste
+    }
+
+    async create(req, res) {
+    // create = async (req, res) => {
+        const car = await this.dao.create(req.body)
+        return res.status(201).json(car)
     }
 }
 
-module.exports = new CarsController();
+module.exports = new CarsController(Car);
+// module.exports = new CarsController(); //v1
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 const carsController = require('../controllers/CarsController')
 
 router.get('/cars', carsController.index)
+router.post('/cars', carsController.create)
 
 module.exports = router
